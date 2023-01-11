55a4a636b1137712ac056eacc758e062266536e1
Michael Wells <info@sygnal.com>
2023-01-11 16:01:20 +1300

    User

---
+++ netlify/edge-functions/user.ts
@@ -0,0 +1,40 @@
+import type { Context } from "https://edge.netlify.com";
+
+export default async (request: Request, context: Context) => {
+  const url = new URL(request.url);
+/*
+  switch (url.searchParams.get("action")) {
+    case "set":
+      context.cookies.set({
+        name: "action",
+        value: "hello",
+      });
+
+      return new Response('Cookie value has been set. Reload this page without the "action" parameter to see it.');
+
+    case "clear":
+      context.cookies.delete("action");
+
+      return new Response(
+        'Cookie value has been cleared. Reload this page without the "action" parameter to see the new state.',
+      );
+  }
+*/
+
+  const value = context.cookies.get("userData");
+
+  if (!value) 
+    return new Response("No user logged in."); 
+
+  const userData = JSON.parse(value);
+//  const message = value
+//    ? `Cookie value is "${userData.email}"".`
+//    : 'Cookie has not been set. You can do so by adding "?action=set" to the URL.';
+
+    return new Response(userData, {
+        headers: { "content-type": "application/json" },
+        }); 
+
+
+//  return new Response(message);
+};
